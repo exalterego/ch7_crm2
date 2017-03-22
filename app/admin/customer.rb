f335c5c824c22d954318ea55e94f36a327136142
Lena <lena.studex@gmail.com>
2017-03-22 16:02:17 -0500

    Part 2: Customer scss, finder controller

--- app/admin/customer.rb
+++ app/admin/customer.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Customer do
-  permit_params  :fullname, :phone, :email, :image, :notes
+  permit_params  :fullname, :pnone, :email, :image, :notes
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
---
+++ app/controllers/finder_controller.rb
@@ -0,0 +1,13 @@
+class FinderController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+
+  def alphabetized
+    @customers = Customer.order(fullname)
+  end
+
+  def missing_email
+    @customers = Customer.where(email: '')
+  end
+end
